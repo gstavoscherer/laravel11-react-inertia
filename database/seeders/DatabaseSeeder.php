450dc7231633bd6058c2e5535507ffa6e883083b
Gustavo Scherer <gst.scherer@gmail.com>
2025-02-23 22:30:53 -0300

    commit2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,14 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Gustavo',
+            'email' => 'gst@example.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now()
         ]);
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
     }
 }
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -29,7 +29,23 @@ export default function AuthenticatedLayout({ header, children }) {
                                     active={route().current('dashboard')}
                                 >
                                     Dashboard
+                                </NavLink><NavLink
+                                    href={route('project.index')}
+                                    active={route().current('project.index')}
+                                >
+                                    Projects
+                                </NavLink><NavLink
+                                    href={route('task.index')}
+                                    active={route().current('task.index')}
+                                >
+                                    All Tasks
+                                </NavLink><NavLink
+                                    href={route('user.index')}
+                                    active={route().current('user.index')}
+                                >
+                                    Users
                                 </NavLink>
+
                             </div>
                         </div>
 
