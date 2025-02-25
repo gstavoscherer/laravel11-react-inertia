970e7d164d59cfda67d42c805f29ff4339dcd160
gustavo <gustavo@DESKTOP-0VR9PC9>
2025-02-25 16:51:56 -0300

    commit

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -18,7 +18,9 @@ export default function Index({auth, projects}){
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              Projects
+              <pre>
+                {JSON.stringify(projects, undefined, 2)}
+              </pre>
             </div>
           </div>
         </div>
